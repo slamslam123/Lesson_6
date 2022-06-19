00f459a69063b4ea38503ab07597e62854602d9a
Никита Киреев <slamslam123@vk.com>
2022-06-19 15:45:31 +0300

    Исправлено

--- Lesson_6/Program.cs
+++ Lesson_6/Program.cs
@@ -1,50 +1,85 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
-namespace Lesson_6
+namespace Lesson_6_new
 {
     class Program
     {
-        static string[] Read()
+        static void ReadDataStaff()
         {
-            string[] lines = File.ReadAllLines(@"C:\SkillBox\data.txt");
-            for (int i = 0; i < lines.Length; i++)
+            using (StreamReader sr = new StreamReader(@"C:\SkillBox\data.txt"))
             {
-                Console.WriteLine($"{lines[i]}");
+                string line;
+                string[] pattern = new string[]
+            { "ID: ",
+              "дата и время: ",
+              "Ф.И.О: ",
+              "возраст: ",
+              "рост: ",
+              "дата рождения: ",
+              "место рождения: "};
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] data = line.Split('#');
+                    Console.Write($"{pattern[0]}{data[0]}" + " ");
+                    Console.Write($"{pattern[1]}{data[1]}" + " ");
+                    Console.Write($"{pattern[2]}{data[2]}" + " ");
+                    Console.Write($"{pattern[3]}{data[3]}" + " ");
+                    Console.Write($"{pattern[4]}{data[4]}" + " ");
+                    Console.Write($"{pattern[5]}{data[5]}" + " ");
+                    Console.Write($"{pattern[6]}{data[6]}");
+                    Console.WriteLine();
+                }
+                Console.ReadKey();
             }
-            Console.ReadKey();
-            return lines;
         }
-        static string[] Write(string[] a)
+        static void WriteDataStaff()
         {
-            File.AppendAllLines(@"C:\SkillBox\data.txt", a);
-            return a;
+            using (StreamWriter sw = new StreamWriter(@"C:\SkillBox\data.txt", true, Encoding.Unicode))
+            {
+                string info = string.Empty;
+                Console.WriteLine("Введите ID:");
+                info += $"{Console.ReadLine()}" + "#";
+
+                string timenow = DateTime.Now.ToString();
+                Console.WriteLine($"Введите Дату и время добавления записи:\n{timenow}");
+                info += $"{(timenow)}" + "#";
+
+                Console.WriteLine("Введите Ф.И.О:");
+                info += $"{Console.ReadLine()}" + "#";
+
+                Console.WriteLine("Введите возраст:");
+                info += $"{Console.ReadLine()}" + "#";
+
+                Console.WriteLine("Введите рост:");
+                info += $"{Console.ReadLine()}" + "#";
+
+                Console.WriteLine("Введите дату рождения:");
+                info += $"{Console.ReadLine()}" + "#";
+
+                Console.WriteLine("Введите место рождения:");
+                info += $"{Console.ReadLine()}";
+                sw.WriteLine(info);
+                Console.ReadKey();
+            }
         }
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.Write("СПРАВОЧНИК СОТРУДНИКОВ.\nВведите 1 / 2 \n1 — вывести данные на экран\n2 — заполнить данные и добавить новую запись \nВвод:");
-                int choice = int.Parse(Console.ReadLine());
-                string[] arrayData = new string[] { "Введите ID:", "Введите Дату и время добавления записи:", "Введите Ф.И.О:", "Введите возраст:", "Введите рост:", "Введите дату рождения:", "Введите место рождения:" };
-                string[] arrayNewdata = new string[arrayData.Length];
                 Console.Clear();
+                Console.Write("Введите 1 или 2.\n1 — вывести данные на экран\n2 — заполнить данные и добавить новую запись \nВвод: ");
+                int choice = int.Parse(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        Read();
+                        ReadDataStaff();
                         break;
                     case 2:
-                        for (int i = 0; i <= arrayData.Length - 1; i++)
-                        {
-                            Console.WriteLine($"{arrayData[i]}");
-                            string a = Console.ReadLine();
-                            arrayNewdata[i] = a;
-                        }
-                        Write(arrayNewdata);
+                        WriteDataStaff();
                         break;
                 }
-                Console.Clear();
             }
         }
     }
